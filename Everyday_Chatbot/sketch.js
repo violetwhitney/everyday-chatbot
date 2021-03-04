890a97cf7b888aaeecb8238b6871a0a46ad39813
Violet Whitney <violet.whitney3@gmail.com>
2021-03-04 18:18:11 -0500

    Add files via upload

--- Everyday_Chatbot/sketch.js
+++ Everyday_Chatbot/sketch.js
@@ -1,7 +1,6 @@
 let dataServer;
-let pubKey = 'pub-c-d41726c0-2286-4bcb-ab26-816b7aff2f67';
-// let subKey = 'sub-c-37825dea-68bc-11eb-9783-9247c8d3dabd';
-let subKey = 'sub-c-00b76468-68bc-11eb-9783-9247c8d3dabd';
+let pubKey = config.pubKey;
+let subKey = config.subKey;
 
 let sendText;
 let sendButton;
@@ -57,7 +56,6 @@ function sendTheMessage() {
   dataServer.publish({
       channel: channelName,
       message: {
-        // who: whoAreYou.value(),
         messageText: sendText.value()  
       }
     });
